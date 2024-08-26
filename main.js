8454cdbb6fed79ba26799d400034375bc1207fa0
vitorroqueandrade10 <130611483+vitorroqueandrade10@users.noreply.github.com>
2024-08-26 20:02:09 -0300

    26/08/2024

--- main.js
+++ main.js
@@ -1,26 +1,7 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");s''''
-,,,,,,,,,,
-
-
-
-
-
-,0000000000000000000000000000
-
-
-
-
-
-
-
-
-
-
-
-
+const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
